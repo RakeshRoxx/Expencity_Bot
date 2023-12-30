134fa445e45fc25e0e8f02126c52b08a8660d687
RakeshMandal <rakeshmandal651@gmail.com>
2023-12-30 13:17:59 +0530

    .env

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const Expense = require("./Model/Expense.js");
 const User = require("./Model/User.js");
 
 const bot = new TelegramBot(process.env.TOKEN, {polling : true});
-mongoose.connect("mongodb://127.0.0.1:27017/test", { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoUrl = process.env.MONGOWEB;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log("Connected to MongoDB");
 })
